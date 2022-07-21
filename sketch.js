316d701e4a81153955148e414497f6f0965c82bc
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-07-21 12:54:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+//1. Declare and initialize an array named friends
+var friends=["dimple","palak","jiya","ananya","ishika"];
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+  console.log("My best friends are :");
+
+  //2. print the names in friends array in the console:
+  for(var i=0;i<friends.length;i++){
+console.log(friends[i])
+
+  }
+
+}
+
+function draw() {
+    
+}
+
+    
+
